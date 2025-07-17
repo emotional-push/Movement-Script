b2cf71cc0e8ada7db1c5cc15726ea964c01ba1f1
emotional-push <sharma.devanshu089@gmail.com>
2025-07-17 21:48:50 +0530

    Fix Rigidbody null check and correct movement force application in PlayerMovement script

--- Script/movment.cs
+++ Script/movment.cs
@@ -8,6 +8,10 @@ public class PlayerMovement : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        if (rb == null)
+        {
+            Debug.LogError("No Rigidbody attached to " + gameObject.name + ". PlayerMovement requires a Rigidbody component.");
+        }
     }
 
     void FixedUpdate()
@@ -19,7 +23,8 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetKey(KeyCode.S))
             force -= transform.forward;
         if (Input.GetKey(KeyCode.A))
-            force -= transform.right;
+        if (force != Vector3.zero)
+            rb.AddForce(force.normalized * moveForce);
         if (Input.GetKey(KeyCode.D))
             force += transform.right;
 
